54c94541a9e2374f24cf7e16d3609cbef786822a
Jonatan <jonatane.cisneros@gmail.com>
2018-01-19 23:50:52 +0100

    Create repo

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=ryainairhelper-Web-Deploy.pubxml -p:Password=Ytc9i8R0kTdeaED1J5GrAWMEBpMxx2xoWJEn0CTaPazfwgE8fsH9G88t0Rc8
+
