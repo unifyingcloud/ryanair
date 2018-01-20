4f62c650eac0e67d47b37dda24e8a392c153dbe1
Jonatan <jonatane.cisneros@gmail.com>
2018-01-20 23:42:23 +0100

    ok

--- Dialogs/RyanairDialogTest.cs
+++ Dialogs/RyanairDialogTest.cs
@@ -201,11 +201,14 @@ public static string parsearJSONInfoVuelo (JToken token){
             {
                 
                 await context.PostAsync("Result");
-                /*   
+               
                 WebRequest request = WebRequest.Create("https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/1703b0e2-e00d-466e-8f99-710cfc850299?subscription-key=7cadeb2e13cf4cd3803cc832b6dfcd15&verbose=true&timezoneOffset=0&q" + message.Text);
                 WebResponse response = request.GetResponse();
 
-                String json;
+
+
+                await context.PostAsync("Result");
+                /*   String json;
 
                 using (var sr = new StreamReader(response.GetResponseStream()))
                 {
