b8be2b048be55d9447d60084ab7b341f123e5535
Jonatan <jonatane.cisneros@gmail.com>
2018-01-20 22:10:31 +0100

    jc3

--- Dialogs/RyanairDialogTest.cs
+++ Dialogs/RyanairDialogTest.cs
@@ -68,10 +68,14 @@ public static string parsearJSONInfoVuelo (JToken token){
 
     public static string parsearJSONVuelos (JToken token){
 		string response = "Vuelos:";
-		/*string status = token.SelectToken("flights[0].status.message").ToString();
+            try{ String status = token.SelectToken("flights[0].status.message").ToString();
 			
 		response += "El vuelo número " + token.SelectToken("flights[0].number").ToString() + " está " + status +".\n\n";	
-*/
+            }
+            catch(Exception ex){
+
+                response = ex.Message;
+            }
 		return response;
 	}
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
